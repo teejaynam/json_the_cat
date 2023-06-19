f33368fd9479a389336f676ce939365bc11f3ac5
TJ <teejaynam@gmail.com>
2023-06-18 21:01:30 -0400

    update to wrap in function to take breed name and callback, also remove all console.logs, and export function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,25 @@
 const request = require('request');
-const breed = process.argv[2];
-const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(URL, (error, response, body) => {
-  
-  if (response.statusCode === 200) {
+const fetchBreedDescription = function(breedName, callback) {
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  let description;
+  let errorDescription;
+
+  request(URL, (error, response, body) => {
+
     const data = JSON.parse(body);
-    if (data.length > 0) {
-      console.log(data[0].name);
-      console.log(data[0].description);
-    } else {
-      console.log(`Breed ${breed} is not found`);
-    }
-  } else {
-    console.log('Request failed. Error:',error);
-  }
 
-});
+    if (data.length > 0 && error === null) {
+      description = data[0].description;
+      errorDescription = null;
+      callback(errorDescription, description);
+    } else {
+      description = null;
+      errorDescription = body; //when set to "Error", even if you put in a bad breed name, it returns with code 200 + "null"
+      callback(errorDescription, description);
+    }
+
+  });
+};
+
+module.exports = { fetchBreedDescription };
